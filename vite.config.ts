18c019edb60e0a315fa957b81f0dfb15c6afe4d4
beampcrp <beampcrp920@gmail.com>
2026-03-09 18:19:03 +0700

    CommitNew

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,20 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MobileWebApp_Project/',
+
   plugins: [
     vue(),
     legacy()
   ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   test: {
     globals: true,
     environment: 'jsdom'
